4b6e98af6d6e80f45d53ed6a9bd158bf53a1f80b
Nazarii Vladyka <vladyka.nazarii@gmail.com>
2025-01-05 17:23:07 +0200

    fix(firebase): remove api key

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,15 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "wish-list-28b8f", appId: "1:69191109318:web:b14e76198f8e88295525c7", storageBucket: "wish-list-28b8f.firebasestorage.app", apiKey: "AIzaSyBV2TFEfTzq47X9_qNfr_fJuJ2aVXfdeWw", authDomain: "wish-list-28b8f.firebaseapp.com", messagingSenderId: "69191109318" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+  ],
 };
